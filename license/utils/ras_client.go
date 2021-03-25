ee18f60618985b59502d0b71e9905612b1324380
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2021-03-25 14:17:50 +0800

    代码优化

--- license/utils/ras_client.go
+++ license/utils/ras_client.go
@@ -4,7 +4,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
-	gorsa "github.com/JiangJibo/ip-fastclient-go/license/gorsa"
+	"github.com/JiangJibo/ip-fastclient-go/license/gorsa"
 )
 
 // 使用公钥解密
